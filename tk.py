8f33e721ef26d97c77063ebe1c6856dd91be3b20
404notsurrender <49116001+404notsurrender@users.noreply.github.com>
2021-09-30 16:11:25 +0700

    Create tk.py

---
+++ tk.py
@@ -0,0 +1,10 @@
+## ISENG AJ
+
+value =input("Please input your age:")
+try:
+   value = 2021-value 
+except:
+    value = 2021-int(value)
+    print(f"Tahun lahir lu{value}")
+
+print (f"Tahun lu {value}")
